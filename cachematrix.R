cedfbc7fcb3837d483ae9c2a608599b6e318f3fb
Ana Paula Januário <ferrari.januario@gmail.com>
2023-10-10 17:52:33 +0100

    Update cachematrix.R
    Ana-Januario-patch-1

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,22 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+cacheSolve <- function(x, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached result")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
+}
+
+#checking
+
+m <- matrix(rnorm(4),2,2)
+m1 <- makeCacheMatrix(m)
+cacheSolve(m1)
